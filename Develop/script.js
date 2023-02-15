345809bbd602652ce410f3f2f19b61108a0972ca
Caleb Lockwood <caleblockwood@iMac.local>
2023-02-14 20:54:30 -0700

    began work on saveBtn function

--- Develop/script.js
+++ Develop/script.js
@@ -4,10 +4,28 @@
 $(function () {
   // TODO: Add a listener for click events on the save button. This code should
   // use the id in the containing time-block as a key to save the user input in
-  // local storage. HINT: What does `this` reference in the click listener
+  // local storage.
+  
+  // HINT: What does `this` reference in the click listener
   // function? How can DOM traversal be used to get the "hour-x" id of the
   // time-block containing the button that was clicked? How might the id be
   // useful when saving the description in local storage?
+
+$('.saveBtn').click(function() {
+
+
+//Gets the text area.
+//$(this) = JjQuery object that wraps around 'this', 'this' references the current object = (.saveBtn)
+//.closest('.time-block) = searchs for and runs the nearest ancestor '.time-block' element of clicked .saveBtn
+//.find('.description') = finds the descendant of the current element (.time-block in this case) that matches the .description element.
+  var textarea = $(this).closest('.time-block').find('.description');
+});
+
+
+
+
+
+
   //
   // TODO: Add code to apply the past, present, or future class to each time
   // block by comparing the id to the current hour. HINTS: How can the id
