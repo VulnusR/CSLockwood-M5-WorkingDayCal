05d96b272c98d97d53de899a7d60319ba46c573d
Caleb Lockwood <caleblockwood@iMac.local>
2023-02-14 21:27:55 -0700

    added variable that tracks the present time using days.js as the hint specified

--- Develop/script.js
+++ Develop/script.js
@@ -6,10 +6,8 @@ $(function () {
   // use the id in the containing time-block as a key to save the user input in
   // local storage.
   
-  // HINT: What does `this` reference in the click listener
-  // function? How can DOM traversal be used to get the "hour-x" id of the
-  // time-block containing the button that was clicked? How might the id be
-  // useful when saving the description in local storage?
+  // HINT: What does `this` reference in the click listener function? How can DOM traversal be used to get the "hour-x" id of the
+  // time-block containing the button that was clicked? How might the id be useful when saving the description in local storage?
 
 
 
@@ -34,35 +32,21 @@ $('.saveBtn').click(function() {
 
  // stores the time & associated text in local storage
 localStorage.setItem(savedTime, descriptionInput);
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 });
 
-
-
-
-
-
-  //
   // TODO: Add code to apply the past, present, or future class to each time
-  // block by comparing the id to the current hour. HINTS: How can the id
-  // attribute of each time-block be used to conditionally add or remove the
-  // past, present, and future classes? How can Day.js be used to get the
-  // current hour in 24-hour time?
+  // block by comparing the id to the current hour. 
+
+  //HINTS: How can the id attribute of each time-block be used to conditionally add or remove the
+  // past, present, and future classes? How can Day.js be used to get the current hour in 24-hour time?
+
+
+// Gets current hour in 24 hour time using "hour" property of days.js, this can be used to help apply past/present/future classes
+var presentTime =dayjs().hour();
+
+
+
+
   //
   // TODO: Add code to get any user input that was saved in localStorage and set
   // the values of the corresponding textarea elements. HINT: How can the id
